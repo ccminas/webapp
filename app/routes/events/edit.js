a332cce0af43864b848dcd664c01e489fbbe7fb4
Kamilla <holanda.kamilla@gmail.com>
2016-10-01 12:17:31 -0300

    Iniciando edição de eventos

--- app/routes/events/edit.js
+++ app/routes/events/edit.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+    return this.store.findRecord('event', params.event_id);
+  },
+
+  saveEvent(newEvent) {
+    newEvent.save().then(() => this.transitionTo('events'));
+  },
+
 });
